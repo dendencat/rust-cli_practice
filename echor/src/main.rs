e6f3639bab2f0d07ab13b6174912a341ab7a92ac
dev-sabatarou <sinkuma135@gmail.com>
2024-09-24 23:17:01 +0900

    UPDATE: clapのtrait追加

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,9 @@
+use clap::App;
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let _matches = App::new("echor")
+    .version("0.1.0")
+    .author("Ken Youens-Clark <kyclark@gmail.com>")
+    .about("Rust echo")
+    .get_matches();
 }
